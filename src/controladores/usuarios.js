f5446c4ad39774d0d81d919bc48fdc81675536c8
christianebs <christianebs90@gmail.com>
2024-01-16 18:48:18 -0300

    Criação das funções de usuario e rotas

--- src/controladores/usuarios.js
+++ src/controladores/usuarios.js
@@ -1,14 +1,59 @@
 const knex = require("../conexao");
 
 const cadastrarUsuario = async (req, res) => {
+  const { nome, email } = req.body;
+  const senhaCriptografada = req.senhaCriptografada;
+
   try {
+    const usuario = await knex("usuarios")
+      .insert({
+        nome,
+        email,
+        senha: senhaCriptografada,
+      })
+      .returning("*");
+
+    if (!usuario[0]) {
+      return res.status(400).json({ mensagem: "Usuário não cadastrado." });
+    }
+
+    const { senha: senhaUsuario, ...novoUsuario } = usuario[0];
+
+    return res.status(201).json({
+      mensagem: "Usuário cadastrado com sucesso:",
+      Usuario: novoUsuario,
+    });
   } catch (error) {
     return res.status(500).json({ mensagem: "Erro interno do servidor." });
   }
 };
 
 const atualizarUsuario = async (req, res) => {
+  const { nome, email } = req.body;
+  const senhaCriptografada = req.senhaCriptografada;
+
   try {
+    const usuario = await knex("usuarios")
+      .where({ id: req.usuario.id })
+      .update({
+        nome,
+        email,
+        senha: senhaCriptografada,
+      });
+
+    if (!usuario) {
+      return res.status(400).json({ mensagem: "Usuário não atualizado." });
+    }
+
+    const usuarioAtualizado = await knex("usuarios")
+      .where({ id: req.usuario.id })
+      .first()
+      .select("id", "nome", "email");
+
+    return res.status(200).json({
+      mensagem: "Usuário atualizado com sucesso:",
+      Usuario: usuarioAtualizado,
+    });
   } catch (error) {
     return res.status(500).json({ mensagem: "Erro interno do servidor." });
   }
@@ -16,6 +61,11 @@ const atualizarUsuario = async (req, res) => {
 
 const detalharUsuario = async (req, res) => {
   try {
+    const { senha, ...usuarioDetalhado } = usuario;
+
+    return res
+      .status(200)
+      .json({ mensagem: "Informações do usuário:", Usuario: usuarioDetalhado });
   } catch (error) {
     return res.status(500).json({ mensagem: "Erro interno do servidor." });
   }
--- src/rotas.js
+++ src/rotas.js
@@ -1,9 +1,24 @@
 const express = require("express");
 
 const { listarCategorias } = require("./controladores/categorias");
+const { cadastrarUsuario, atualizarUsuario, detalharUsuario } = require("./controladores/usuarios");
+const { login } = require("./controladores/login");
+
+const { validarCamposObrigatoriosUsuario, validarEmailUsuario } = require("./intermediarios/validacoes");
+
+const { criptografarSenha } = require("./intermediarios/criptografarSenha");
+const { validarLogin } = require("./intermediarios/autenticacao");
 
 const rotas = express();
 
 rotas.get("/categoria", listarCategorias);
+
+rotas.post("/usuario", validarCamposObrigatoriosUsuario, criptografarSenha, validarEmailUsuario, cadastrarUsuario);
+rotas.post("/login", login);
+
+rotas.use(validarLogin);
+
+rotas.get("/usuario", detalharUsuario);
+rotas.put("/usuario", validarCamposObrigatoriosUsuario, criptografarSenha, validarEmailUsuario, atualizarUsuario);
 
 module.exports = rotas;
