541c234ec182f3cb35fc02d86e98afd2f9aeccee
christianebs <christianebs90@gmail.com>
2024-01-17 20:36:32 -0300

    Desenvolvimento das funções de produto

--- src/rotas.js
+++ src/rotas.js
@@ -1,17 +1,23 @@
 const express = require("express");
-
-const { listarCategorias } = require("./controladores/categorias");
-const { cadastrarUsuario, atualizarUsuario, detalharUsuario } = require("./controladores/usuarios");
+const multer = require("./intermediarios/multer");
 const { login } = require("./controladores/login");
-
-const { validarCamposObrigatoriosUsuario, validarEmailUsuario } = require("./intermediarios/validacoes");
-
+const { cadastrarCategoria, listarCategorias } = require("./controladores/categorias");
+const { cadastrarUsuario, atualizarUsuario, detalharUsuario } = require("./controladores/usuarios");
+const { cadastrarProduto, editarDadosProduto, detalharProduto, deletarProduto, listarProdutos } = require("./controladores/produtos");
 const { criptografarSenha } = require("./intermediarios/criptografarSenha");
 const { validarLogin } = require("./intermediarios/autenticacao");
+const {
+  validarCamposObrigatoriosUsuario,
+  validarEmailUsuario,
+  validarCamposObrigatoriosProduto,
+  validarIdProduto,
+  validarIdCategoria,
+} = require("./intermediarios/validacoes");
 
 const rotas = express();
 
 rotas.get("/categoria", listarCategorias);
+rotas.post("/categoria", cadastrarCategoria);
 
 rotas.post("/usuario", validarCamposObrigatoriosUsuario, criptografarSenha, validarEmailUsuario, cadastrarUsuario);
 rotas.post("/login", login);
@@ -20,5 +26,11 @@ rotas.use(validarLogin);
 
 rotas.get("/usuario", detalharUsuario);
 rotas.put("/usuario", validarCamposObrigatoriosUsuario, criptografarSenha, validarEmailUsuario, atualizarUsuario);
+
+rotas.post("/produto", multer.single("produto_imagem"), validarCamposObrigatoriosProduto, validarIdCategoria, cadastrarProduto );
+rotas.put("/produto/:id", multer.single("produto_imagem"), validarCamposObrigatoriosProduto, validarIdProduto, validarIdCategoria, editarDadosProduto);
+rotas.get("/produto", validarIdCategoria, listarProdutos);
+rotas.get("/produto/:id", validarIdProduto, detalharProduto);
+rotas.delete("/produto/:id",validarIdProduto, deletarProduto);
 
 module.exports = rotas;
